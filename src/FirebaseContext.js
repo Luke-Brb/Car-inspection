2af54eb891fe6e8e6f4484198396e4a870a26d32
Lucian Barbu <112772067+Luke-Brb@users.noreply.github.com>
2025-01-15 13:12:23 +0200

    Update FirebaseContext.js

--- src/FirebaseContext.js
+++ src/FirebaseContext.js
@@ -2,7 +2,6 @@ import React, { createContext, useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getFunctions, httpsCallable } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwYuMsDLJe7uKxunJc2OasfTcYiN5XgKY",
